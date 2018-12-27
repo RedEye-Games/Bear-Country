505768e55d11225b38cac6622f4269dcae6be96d
Nicky Young <nicholas.eng.young@gmail.com>
2018-12-27 16:25:32 -0500

    added tile collider logic, initial mouse drag

---
+++ Assets/Scripts/tileCollider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tileCollider : MonoBehaviour {
+
+    public bool isEmpty = true;
+    Color colorStart = Color.red;
+    Color colorEnd = Color.green;
+    Renderer rend;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+   
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag ("Tile") && isEmpty)
+        {
+            print("Tile Collided");
+            rend.material.color = Color.green;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        rend.material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if {
+            
+        }
+
+    }
+}

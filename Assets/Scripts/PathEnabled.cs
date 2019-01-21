513f86e8e2a9e4f98fcd9ac0523061df303cca89
chris mitchell <spacehelmetboy@gmail.com>
2019-01-21 23:49:04 +0700

    created tile prefab variants

---
+++ Assets/Scripts/PathEnabled.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public enum Orientation { _0, _90, _180, _270 }
+
+public class PathEnabled : MonoBehaviour
+{
+    public Orientation orientation;
+    public PathPoint[] pathPointList;
+}
+
+[Serializable]
+public class PathPoint
+{
+    public enum PathPointType { River, Trail };
+    public PathPointType pathPointType;
+
+    public enum PathPointEdge { top, right, bottom, left };
+    public PathPointEdge edge;
+}

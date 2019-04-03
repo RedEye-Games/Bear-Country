ab7ddc0199b33417aecc80c66f65e3184e7fca13
Lukas Fauset <lfauset@gmail.com>
2019-04-02 20:32:57 -0400

    Rudimentary Loading Screen

--- Assets/Scripts/Buttons/LoadSceneOnClick.cs
+++ Assets/Scripts/Buttons/LoadSceneOnClick.cs
@@ -5,8 +5,11 @@ using UnityEngine.SceneManagement;
 public class LoadSceneOnClick : MonoBehaviour
 {
 
+    public Canvas loadingScreen;
+
     public void LoadByIndex(int sceneIndex)
     {
+        loadingScreen.enabled = true;
         SceneManager.LoadScene(sceneIndex);
     }
 }
